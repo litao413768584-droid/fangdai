3d7ac72f3ebce944b102574e05c00a0c0a00bde6
石油计量 <litao413768584@gmail.com>
2026-07-31 10:59:03 +0800

    Tailwind（快速修复）

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
+    postcss: './postcss.config.js',
   },
 })
