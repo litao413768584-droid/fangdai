e5951ddedb1adb6314a03f16033bb881b86f2a25
石油计量 <litao413768584@gmail.com>
2026-07-31 09:39:33 +0800

    修改 vite.config.ts 指定 postcss 路径

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 ﻿/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 让 Vite 自动处理 CSS
+  css: {
+    postcss: './postcss.config.cjs',
+  },
 })
